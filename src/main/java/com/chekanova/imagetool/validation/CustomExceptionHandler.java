f6808fdf40095c29e276cef4765a4803c15d2862
Natanius18 <leonidchachko17@gmail.com>
2022-07-20 17:35:06 +0300

    Add error output to JSON

---
+++ src/main/java/com/chekanova/imagetool/validation/CustomExceptionHandler.java
@@ -0,0 +1,24 @@
+package com.chekanova.imagetool.validation;
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.io.IOException;
+import java.util.Date;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+@ControllerAdvice
+public class CustomExceptionHandler {
+    @ExceptionHandler({IOException.class, InterruptedException.class, IllegalArgumentException.class})
+    protected ResponseEntity<Object> handleConflict(RuntimeException ex) {
+        Map<String, Object> body = new LinkedHashMap<>();
+        body.put("timestamp", new Date());
+        body.put("status", HttpStatus.BAD_REQUEST.value());
+        body.put("error", ex.getMessage());
+        return new ResponseEntity<>(body, new HttpHeaders(), HttpStatus.BAD_REQUEST);
+    }
+}

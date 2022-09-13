1f57317c83efff02e1491b2b35806d65849764f9
oleksandra.chekanova <oleksandra.chekanova@sigma.software>
2022-09-14 01:12:42 +0300

    IMG-8 Refactor all tests using parameters

--- src/test/java/com/chekanova/imagetool/service/comparison/impl/ImageComparisonServiceImplTest.java
+++ src/test/java/com/chekanova/imagetool/service/comparison/impl/ImageComparisonServiceImplTest.java
@@ -1,56 +1,59 @@
 package com.chekanova.imagetool.service.comparison.impl;
 
-import org.junit.Before;
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.api.extension.ExtensionContext;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.ArgumentsProvider;
+import org.junit.jupiter.params.provider.ArgumentsSource;
 import org.mockito.InjectMocks;
-import org.mockito.junit.MockitoJUnitRunner;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
+import java.util.stream.Stream;
 
-import static org.junit.Assert.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 
-@RunWith(MockitoJUnitRunner.class)
-public class ImageComparisonServiceImplTest {
-    public static final String SOURCE_FILE = "src/test/resources/";
+@ExtendWith(MockitoExtension.class)
+class ImageComparisonServiceImplTest {
+    private static final String SOURCE_FILE = "src/test/resources/";
 
     @InjectMocks
-    private final ImageComparisonServiceImpl testedObject = new ImageComparisonServiceImpl();
+    private ImageComparisonServiceImpl testedObject;
 
-    private BufferedImage image1;
-    private BufferedImage image2;
-    private BufferedImage image3;
+    private static final boolean[][] comparisonWithDiff = {{false, false, false, false, false},
+            {false, false, false, false, false},
+            {false, false, true, false, false},
+            {false, false, false, false, false},
+            {false, false, false, false, false}};
 
-    @Before
-    public void setUp() throws IOException {
-        image1 = ImageIO.read(new File(SOURCE_FILE + "image1.png"));
-        image2 = ImageIO.read(new File(SOURCE_FILE + "image2.png"));
-        image3 = ImageIO.read(new File(SOURCE_FILE + "image3.png"));
-    }
+    private static final boolean[][] comparisonWithoutDiff = {{false, false, false, false, false},
+            {false, false, false, false, false},
+            {false, false, false, false, false},
+            {false, false, false, false, false},
+            {false, false, false, false, false}};
 
-    @Test
-    public void testCompareSimilarColors() {
-        boolean[][] expected = {{false, false, false, false, false},
-                {false, false, false, false, false},
-                {false, false, false, false, false},
-                {false, false, false, false, false},
-                {false, false, false, false, false}};
-
-        boolean[][] actual = testedObject.compare(image3, image2);
-        assertArrayEquals(actual, expected);
-    }
-
-    @Test
-    public void testCompareDifferentColors() {
-        boolean[][] expected = {{false, false, false, false, false},
-                {false, false, false, false, false},
-                {false, false, true, false, false},
-                {false, false, false, false, false},
-                {false, false, false, false, false}};
+    @DisplayName("Test image comparison")
+    @ParameterizedTest(name = "{index}, {0}")
+    @ArgumentsSource(ImageComparisonArgumentsProvider.class)
+    void testCompare(String message, String fileName1, String fileName2, boolean[][] expected) throws IOException {
+        BufferedImage image1 = ImageIO.read(new File(SOURCE_FILE + fileName1));
+        BufferedImage image2 = ImageIO.read(new File(SOURCE_FILE + fileName2));
         boolean[][] actual = testedObject.compare(image1, image2);
-        assertArrayEquals(actual, expected);
+        assertArrayEquals(expected, actual);
+    }
+
+    static class ImageComparisonArgumentsProvider implements ArgumentsProvider {
+        @Override
+        public Stream<? extends Arguments> provideArguments(ExtensionContext extensionContext) {
+            return Stream.of(
+                    Arguments.of("Should return comparison result for similar images", "image3.png", "image2.png", comparisonWithoutDiff),
+                    Arguments.of("Should return comparison result for different images", "image1.png", "image2.png", comparisonWithDiff)
+            );
+        }
     }
 }
